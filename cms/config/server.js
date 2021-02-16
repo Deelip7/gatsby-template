cd8e5d2e9afffb447d4b09c9567bf4e4977c26b9
Theelipan prabakar <theelipan@gmail.com>
2021-02-15 22:16:26 -0500

    install strapi

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '39c64597e626ce47517b896ba7983554'),
+    },
+  },
+});
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a9d178ea-d354-4e05-b7df-1c4c726137ef'
+};
