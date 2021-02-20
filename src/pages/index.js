23b35e0d32f0757e8418b9333cb91bb0cf047df5
Theelipan prabakar <theelipan@gmail.com>
2021-02-19 19:44:51 -0500

    connect to strapi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,49 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <h1>New Articles</h1>
+    <hr />
+    {data.allStrapiArticle.edges.map(doc => {
+      return (
+        <div key={doc.node.id}>
+          <h1>{doc.node.Title} </h1>
+          <p>{doc.node.content} </p>
+          <p>
+            By {doc.node.author.username}{" "}
+            {doc.node.image.map(img => (
+              <img src={img.url} alt="" />
+            ))}
+          </p>
+        </div>
+      )
+    })}
   </Layout>
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allStrapiArticle {
+      edges {
+        node {
+          id
+          content
+          Title
+
+          image {
+            url
+          }
+          author {
+            username
+          }
+        }
+      }
+    }
+  }
+`
